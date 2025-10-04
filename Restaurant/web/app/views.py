708fbe40ed3cc4ff4d45226a95245de4a89965dc
balamon11 <sangeethathandapani7@gmail.com>
2025-10-04 20:57:42 +0530

    all

--- Restaurant/web/app/views.py
+++ Restaurant/web/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+def menu(request):
+    return render(request,'menu.html')
+def admin(request):
+    return render(request,'admin.html')
+def contact(request):
+    return render(request,'contact.html')
